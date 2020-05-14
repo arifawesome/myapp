9130d9848c431e1a773ba163a508614985f768db
Arif Retiwala <arifretiwala@arifs-air-2.attlocal.net>
2020-05-14 16:14:06 -0700

    updated

--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'g%@puq!8vralr*xe(46y#=7n4ui7pfa0$x2qh1ub(il-h)pb9q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://mysterious-mesa-21689.herokuapp.com']
+ALLOWED_HOSTS = ['mysterious-mesa-21689.herokuapp.com']
 
 
 # Application definition
