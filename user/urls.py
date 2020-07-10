1b48941d0de02ac9be0efc962e61ac3800067d90
Arif Retiwala <arifretiwala@Arifs-MacBook-Air-2.local>
2020-07-10 11:03:41 -0700

    uodates

--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('UserInfo/', views.UserInfoList.as_view()),
-    url(r'^UserInfo/(?P<user_id>\d+)/$', views.UserInfoDetail.as_view()),
+    path('UserInfo/<user_id>/', views.UserInfoDetail.as_view()),
     path('UserTradeInfo/', views.UserTradeInfoList.as_view()),
-    url(r'^UserTradeInfo/(?P<user_id>\d+)/$', views.UserTradeInfoDetail.as_view()),
+    path('UserTradeInfo/<user_id>/', views.UserTradeInfoDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<pk>/', views.UserDetail.as_view()),
 ]
